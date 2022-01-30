7db581e2c6ddfb5d07536d5597c36bc1e76b6336
charliedil <charliedil2019@gmail.com>
2022-01-30 18:45:47 -0500

    One rule down, plus a default response (only one though)

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 #  Hi! This is my ELIZA program. Work in progress for sure.
+import random
+import re
+
 subj = ""
 obj = ""
 name = input("[ELIZA]: Hi! My name is ELIZA. It's nice to meet you. What should I call you?\n")
+test1outputs = ["Why are you", "Is there a reason why you are", "What do you mean when you say you are"]
 
 pronouns = input(f"[ELIZA]: It is very nice to meet you {name}! I am looking forward to getting to know you\n"
             f"Please specify your pronouns in the format: subject/object. \nFor example, "
@@ -11,3 +15,13 @@ while len(pronouns.split("/"))<2:
 
 subj = pronouns.split("/")[0].upper()
 obj = pronouns.split("/")[1].upper()
+usrinput = input(f"[ELIZA]: Thank you, {name}. How can I help you today? Note- when you are ready to end our\nconversation,"
+                 f" please type and enter \"exit\" by itself.\n[{name}]: ")
+while usrinput.upper()!="EXIT":
+    test1 = re.search("^I\sAM(.*)", usrinput.upper())
+    if(test1):
+        predicate = test1.group(1)
+        usrinput=input(f"[ELIZA]: {test1outputs[random.randint(0,2)]}{predicate.lower()}\n[{name}]: ")
+    else:
+        usrinput=input(f"[ELIZA]: I see.\n[{name}]: ")
+print("Goodbye!")

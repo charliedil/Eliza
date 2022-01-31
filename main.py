c566815a14e0107f8a658dd04e15030f81233840
charliedil <charliedil2019@gmail.com>
2022-01-30 22:35:05 -0500

    Rolled back a bunch of rules. Realized that it will be more effective to do a bunch of you and i swaps at the beginning. Will have to write rules again, which is annoying, but oh well, I've got time.

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ import re
 subj = ""
 obj = ""
 name = input("[ELIZA]: Hi! My name is ELIZA. It's nice to meet you. What should I call you?\n")
-test1outputs = ["Why are you", "Is there a reason why you are", "What do you mean when you say you are"]
-test2outputs = ["Tell me more about your", "Your"]
+
+defaultanswers = ["I see", "What else is on your mind?", "I don't really understand"]
 pronouns = input(f"[ELIZA]: It is very nice to meet you {name}! I am looking forward to getting to know you\n"
             f"Please specify your pronouns in the format: subject/object. \nFor example, "
             f"my pronouns are ze/zir. Other examples of pronoun sets are he/him, she/her, and they/them.\n[{name}]:")
@@ -18,17 +18,43 @@ obj = pronouns.split("/")[1].upper()
 usrinput = input(f"[ELIZA]: Thank you, {name}. How can I help you today? Note- when you are ready to end our\nconversation,"
                  f" please type and enter \"exit\" by itself.\n[{name}]: ")
 while usrinput.upper()!="EXIT":
-    test1 = re.search("^I\sAM(.*)", usrinput.upper())
-    test2 = re.search("\s*MY\s(.*)", usrinput.upper())
-    if(test1):
-        predicate = test1.group(1)
-        usrinput=input(f"[ELIZA]: {test1outputs[random.randint(0,2)]}{predicate.lower()}\n[{name}]: ")
-    elif(test2):
-        ra = random.randint(0,2)
-        if ra == 2:
-            usrinput=input(f"[ELIZA]: How do/does your {test2.group(1).lower()} make you feel?")
-        else:
-            usrinput=input(f"[ELIZA]: {test2outputs[ra]} {test2.group(1).lower()}\n[{name}]: ")
+    usrinput = usrinput.upper()
+    usrinput = re.sub("[\.\,\?\!]","", usrinput)
+    usrinput = re.sub("(^I\s)", "YOU ", usrinput)
+    usrinput = re.sub("(\sI\s)", " YOU ", usrinput)
+    usrinput = re.sub("\sI$", " YOU", usrinput)
+    usrinput = re.sub("(^MYSELF\s)", "YOURSELF ", usrinput)
+    usrinput = re.sub("(\sMYSELF\s)", " YOURSELF ", usrinput)
+    usrinput = re.sub("\sMYSELF$", " YOURSELF", usrinput)
+    usrinput = re.sub("(^ME\s)", "YOU ", usrinput)
+    usrinput = re.sub("(\sME\s)", " YOU ", usrinput)
+    usrinput = re.sub("\sME$", " YOU", usrinput)
+    usrinput = re.sub("(^MY\s)", "YOUR ", usrinput)
+    usrinput = re.sub("(\sMY\s)", " YOUR ", usrinput)
+    usrinput = re.sub("\sMY$", " YOUR", usrinput)
+    usrinput = re.sub("(^YOURSELF\s)", "MYSELF ", usrinput)
+    usrinput = re.sub("(\sYOURSELF\s)", " MYSELF ", usrinput)
+    usrinput = re.sub("\sYOURSELF$", " MYSELF", usrinput)
+    usrinput = re.sub("(^YOU\s)", "I ", usrinput)
+    usrinput = re.sub("(\sYOU\s)", " I ", usrinput)
+    usrinput = re.sub("\sYOU$", " ME", usrinput)
+    usrinput = re.sub("(^YOUR\s)", "MY ", usrinput)
+    usrinput = re.sub("(\sYOUR\s)", " MY ", usrinput)
+    usrinput = re.sub("\sYOUR$", " MY", usrinput)
+    usrinput = re.sub("(^ARE\s)", "YOURSELF ", usrinput)
+    usrinput = re.sub("(\sMYSELF\s)", " YOURSELF ", usrinput)
+    usrinput = re.sub("\sMYSELF$", " YOURSELF", usrinput)
+
+    print(usrinput)
+    being_test_present = re.search("^YOU\sAM(.*)", usrinput)
+    being_test_past = re.search("^YOU\sWAS(.*)", usrinput)
+    being_test_future = re.search("^YOU\sWILL(.*)", usrinput)
+    rev_being_test_present = re.search("^I\sARE(.*)", usrinput)
+    rev_being_test_past = re.search("^I\sWERE(.*)", usrinput)
+    rev_being_test_future = re.search("^I\sWILL(.*)", usrinput)
+    if(False):
+        print("")
+
     else:
         usrinput=input(f"[ELIZA]: I see.\n[{name}]: ")
 print("Goodbye!")

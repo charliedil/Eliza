4cf1f689d5adeee64ca510fb68dd321a1a8104fa
charliedil <charliedil2019@gmail.com>
2022-01-30 19:09:23 -0500

    Made a second rule for "My".

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ subj = ""
 obj = ""
 name = input("[ELIZA]: Hi! My name is ELIZA. It's nice to meet you. What should I call you?\n")
 test1outputs = ["Why are you", "Is there a reason why you are", "What do you mean when you say you are"]
-
+test2outputs = ["Tell me more about your", "Your"]
 pronouns = input(f"[ELIZA]: It is very nice to meet you {name}! I am looking forward to getting to know you\n"
             f"Please specify your pronouns in the format: subject/object. \nFor example, "
             f"my pronouns are ze/zir. Other examples of pronoun sets are he/him, she/her, and they/them.\n[{name}]:")
@@ -19,9 +19,16 @@ usrinput = input(f"[ELIZA]: Thank you, {name}. How can I help you today? Note- w
                  f" please type and enter \"exit\" by itself.\n[{name}]: ")
 while usrinput.upper()!="EXIT":
     test1 = re.search("^I\sAM(.*)", usrinput.upper())
+    test2 = re.search("\s*MY\s(.*)", usrinput.upper())
     if(test1):
         predicate = test1.group(1)
         usrinput=input(f"[ELIZA]: {test1outputs[random.randint(0,2)]}{predicate.lower()}\n[{name}]: ")
+    elif(test2):
+        ra = random.randint(0,2)
+        if ra == 2:
+            usrinput=input(f"[ELIZA]: How do/does your {test2.group(1).lower()} make you feel?")
+        else:
+            usrinput=input(f"[ELIZA]: {test2outputs[ra]} {test2.group(1).lower()}\n[{name}]: ")
     else:
         usrinput=input(f"[ELIZA]: I see.\n[{name}]: ")
 print("Goodbye!")
